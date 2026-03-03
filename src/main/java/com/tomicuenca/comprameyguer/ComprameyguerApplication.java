f80d8ceef2d7fbf11768c6198547a1d82c12080d
Tomás Cuenca <40648224+tomicuenca@users.noreply.github.com>
2026-03-02 22:18:52 -0300

    Initial commit

---
+++ src/main/java/com/tomicuenca/comprameyguer/ComprameyguerApplication.java
@@ -0,0 +1,13 @@
+package com.tomicuenca.comprameyguer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComprameyguerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComprameyguerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tomicuenca/comprameyguer/ComprameyguerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomicuenca.comprameyguer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComprameyguerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
